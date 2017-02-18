0026a659ee1fb44f4714efd74d07b8099a04852d
Light--- <eleary01@qub.ac.uk>
2017-02-18 23:55:09 +0000

    Adding New Items To Search For

--- spec/features/pages/search_for_item.rb
+++ spec/features/pages/search_for_item.rb
@@ -3,16 +3,40 @@ module Pages
 		include RSpec::Matchers
 		include Capybara::DSL
 
-		def Able_to_enter_in_search_box 
+		def Able_to_enter_in_search_box_helmet
 		click('.nav-search-field')
     	fill_in '.nav-search-field', with: 'helmet'
 		expect('.nav-search-field').to have_css('helmet')
 		end
 
-		def Search_for_an_item_that_is_sold_in_Amazon 
+		def Search_for_an_item_that_is_sold_in_Amazon_helmet
     	click_button('nav-search-submit-text')
 		expect(page).to have_content('search results')
 		expect(page).to have_content('helmet')
+		end
+
+		def Able_to_enter_in_search_box_shirt
+		click('.nav-search-field')
+    	fill_in '.nav-search-field', with: 'shirt'
+		expect('.nav-search-field').to have_css('shirt')
+		end
+
+		def Search_for_an_item_that_is_sold_in_Amazon_shirt
+    	click_button('nav-search-submit-text')
+		expect(page).to have_content('search results')
+		expect(page).to have_content('shirt')
+		end
+
+		def Able_to_enter_in_search_box_pirates
+		click('.nav-search-field')
+    	fill_in '.nav-search-field', with: 'pirates'
+		expect('.nav-search-field').to have_css('pirates')
+		end
+
+		def Search_for_an_item_that_is_sold_in_Amazon_pirates
+    	click_button('nav-search-submit-text')
+		expect(page).to have_content('search results')
+		expect(page).to have_content('pirates')
 		end
 
 		def Search_for_an_item_in_a_department
--- spec/features/search_for_item/search_for_item_spec.rb
+++ spec/features/search_for_item/search_for_item_spec.rb
@@ -9,13 +9,31 @@ RSpec.feature 'Able to search for items' do
     go_to_pages.visit_home_page
   end
 
-  scenario 'Able to enter into search box' do
-    search_for_item.Able_to_enter_in_search_box
-end
+  scenario 'Able to enter helmet into search box' do
+    search_for_item.Able_to_enter_in_search_box_helmet
+  end
 
-   scenario 'Search for an item that is sold in Amazon.co.uk' do
-   	search_for_item.Able_to_enter_in_search_box
-    search_for_item.Search_for_an_item_that_is_sold_in_Amazon
+   scenario 'Search for helmets that is sold in Amazon.co.uk' do
+   	search_for_item.Able_to_enter_in_search_box_helmet
+    search_for_item.Search_for_an_item_that_is_sold_in_Amazon_helmet
+  end
+
+   scenario 'Able to enter shirt into search box' do
+    search_for_item.Able_to_enter_in_search_box_shirt
+  end
+
+   scenario 'Search for shirt that is sold in Amazon.co.uk' do
+    search_for_item.Able_to_enter_in_search_box_shirt
+    search_for_item.Search_for_an_item_that_is_sold_in_Amazon_shirt
+  end
+
+  scenario 'Able to enter pirates into search box' do
+    search_for_item.Able_to_enter_in_search_box_pirates
+  end
+
+   scenario 'Search for pirates that is sold in Amazon.co.uk' do
+    search_for_item.Able_to_enter_in_search_box_pirates
+    search_for_item.Search_for_an_item_that_is_sold_in_Amazon_pirates
   end
 
     scenario 'Search for an item in a particular department' do
